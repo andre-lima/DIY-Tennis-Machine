ce62f3694e1842ea73cb08a45c4b3f0d3d422119
Andre Dantas Lima <andre.dantas.lima@gmail.com>
2020-10-27 19:00:53 +0000

    links for reference

---
+++ README.md
@@ -0,0 +1,10 @@
+# Setting up access point so i can connect from a phone
+
+https://www.raspberrypi.org/documentation/configuration/wireless/access-point-routed.md
+
+Launch express server, connect to SSID from your phone and on the browser try this:
+192.168.4.1:3000
+
+# Game controller demo
+
+Based on this: https://xaviergeerinck.com/post/coding/javascript/xbox-controller/
--- expressserver/server.js
+++ expressserver/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const ip = "192.168.4.1"
 
 app.get('/', (req, res) => {
   console.log('Someone is here...')
   res.send('Hello World!')
 })
 
-app.listen(port, '10.0.0.1', () => {
-  console.log(`Example app listening at http://ip:${port}`)
+app.listen(port, ip, () => {
+  console.log(`Example app listening at http://${ip}:${port}`)
 })
