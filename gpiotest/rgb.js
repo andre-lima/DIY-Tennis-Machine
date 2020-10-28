ff00267031e19a571debeb0ee23a340104824498
Andre Dantas Lima <andre.dantas.lima@gmail.com>
2020-10-28 18:31:25 +0000

    rgb

---
+++ gpiotest/rgb.js
@@ -0,0 +1,14 @@
+const Gpio = require('pigpio').Gpio;
+const red = new Gpio(26, { mode: Gpio.OUTPUT });
+const green = new Gpio(19, { mode: Gpio.OUTPUT });
+const blue = new Gpio(13, { mode: Gpio.OUTPUT });
+
+setInterval(() => {
+  red.pwmWrite(randomByte());
+  green.pwmWrite(randomByte());
+  blue.pwmWrite(randomByte());
+}, 500);
+
+function randomByte() {
+  return 255 - Math.floor(Math.random() * 256);
+}
