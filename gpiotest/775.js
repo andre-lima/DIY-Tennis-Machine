57f23b3960a711159c7fb896623d40d9ad61c9c2
Andre Dantas Lima <andre.dantas.lima@gmail.com>
2020-11-12 16:22:33 +0000

    775 control

--- gpiotest/775.js
+++ gpiotest/775.js
@@ -1,7 +1,7 @@
 const i2c = require('i2c-bus');
 const Gpio = require('pigpio').Gpio;
 
-const PWM_RANGE = 32768;
+const PWM_RANGE = 255;
 const enablePin1 = new Gpio(17, { mode: Gpio.OUTPUT });
 enablePin1.pwmRange(PWM_RANGE)
 const enablePin2 = new Gpio(27, { mode: Gpio.OUTPUT });
@@ -18,14 +18,12 @@ const interval = setInterval(() => {
 
   const rawData1 = i2c1.readWordSync(ADDR, POT1_REG);
   let speed1 = Math.floor(rawData1 * PWM_RANGE / ((2 ** resolution) - 1));
-  enablePin1.pwmRange(32768)
   enablePin1.pwmWrite(speed1);
 
   const rawData2 = i2c1.readWordSync(ADDR, POT2_REG);
   let speed2 = Math.floor(rawData2 * PWM_RANGE / ((2 ** resolution) - 1));
   enablePin2.pwmWrite(speed2);
-  console.log(speed1, speed2)
-}, 100);
+}, 200);
 
 function close() {
   console.log('closing connection...');
